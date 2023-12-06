c5e34fdbf3aa65bc064aa0ce816a57c1c23bf05b
Sean Gray <seangray.dev@gmail.com>
2023-12-06 16:18:28 -0500

    add invoices component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Invoices from '@/components/layout/invoices';
 import NavBar from '@/components/layout/navigation/NavBar';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col 2xl:grid 2xl:grid-cols-[103px_1fr]">
       <NavBar />
+      <Invoices />
     </main>
   );
 }
