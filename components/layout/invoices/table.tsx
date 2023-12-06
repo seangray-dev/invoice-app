e8fde1aee9c97ad8f2beed85a67c3711c37f25ac
Sean Gray <seangray.dev@gmail.com>
2023-12-06 05:09:40 -0500

    move boilerplate code

--- components/layout/invoices/table.tsx
+++ components/layout/invoices/table.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
+import { formatCurrency, formatDateToLocal } from '@/app/lib/utils';
+import {
+  DeleteInvoice,
+  UpdateInvoice,
+} from '@/components/layout/invoices/buttons';
+import InvoiceStatus from '@/components/layout/invoices/status';
+import Image from 'next/image';
 
 export default async function InvoicesTable({
   query,
