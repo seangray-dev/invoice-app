06cac5421ca0a081e91fcc2aab6c66bcbe084868
Sean Gray <seangray.dev@gmail.com>
2023-12-12 16:20:24 -0500

    add invoice/id route

---
+++ app/invoice/[id]/page.tsx
@@ -0,0 +1,12 @@
+import InvoiceDetails from '@/components/layout/invoices/invoice-details';
+import NavBar from '@/components/layout/navigation/NavBar';
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <NavBar />
+      <InvoiceDetails />
+      Invoice {params.id}
+    </>
+  );
+}
