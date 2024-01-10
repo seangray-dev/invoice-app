2e5e369d2d5b6ea5a3d5d647ac2e7e92c8057693
Sean Gray <seangray.dev@gmail.com>
2024-01-09 20:11:56 -0500

    fix ts errors

--- components/layout/invoices/invoice-actions.tsx
+++ components/layout/invoices/invoice-actions.tsx
@@ -1,15 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { Invoice } from '@/lib/definitions';
+import EditInvoice from './edit-invoice';
 
-export default function InvoiceActions() {
+export default function InvoiceActions({ invoice }: { invoice: Invoice }) {
   return (
     <div className="fixed bottom-0 w-full bg-card p-6 dark:bg-nav md:static md:p-0">
       <div className="flex justify-end gap-2">
-        <Button
-          variant={'secondary'}
-          className="w-[73px] rounded-full py-6 font-bold"
-        >
-          Edit
-        </Button>
+        {invoice && <EditInvoice invoice={invoice} />}
         <Button
           variant={'destructive'}
           className="w-[89px] rounded-full py-6 font-bold"
