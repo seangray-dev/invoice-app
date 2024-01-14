084270f049c64e2535f6b6663b5643b71fa43876
Sean Gray <seangray.dev@gmail.com>
2024-01-14 00:39:36 -0500

    modify to use shadcn Inpute + Label components

--- app/components/invoices/search.tsx
+++ app/components/invoices/search.tsx
@@ -3,6 +3,8 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -22,10 +24,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
+      <Label htmlFor="search" className="sr-only">
         Search
-      </label>
-      <input
+      </Label>
+      <Input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
