41c3050ec317427d42c71a68c7f0507b176e962f
Sean Gray <seangray.dev@gmail.com>
2024-01-14 05:43:46 -0500

    remove import * from React

--- app/components/nav/mode-toggle.tsx
+++ app/components/nav/mode-toggle.tsx
@@ -2,7 +2,6 @@
 
 import { MoonIcon, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import * as React from 'react';
 
 import { Button } from '@/app/components/ui/button';
 import {
