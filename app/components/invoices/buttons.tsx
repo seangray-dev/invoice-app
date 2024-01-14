8535f3bbf7185cae99b4768c3acbe8a99bceb2b2
Sean Gray <seangray.dev@gmail.com>
2024-01-14 00:39:00 -0500

    modify CreateInvoice + add ViewInvoice

--- app/components/invoices/buttons.tsx
+++ app/components/invoices/buttons.tsx
@@ -1,16 +1,22 @@
 import { deleteInvoice } from '@/app/lib/actions';
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { ChevronRightIcon, PlusIcon } from 'lucide-react';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export function CreateInvoice() {
   return (
-    <Link
-      href="/dashboard/invoices/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-    >
-      <span className="hidden md:block">Create Invoice</span>{' '}
-      <PlusIcon className="h-5 md:ml-4" />
-    </Link>
+    <Button className="flex w-[90px] items-center gap-2 rounded-full p-4 md:w-[150px] md:gap-4">
+      <div>
+        <div className="-ml-3 flex h-8 w-8 items-center justify-center rounded-full bg-white">
+          <PlusIcon className="h-4 w-4 text-primary" />
+        </div>
+      </div>
+      <div className="flex flex-1 gap-1 font-bold">
+        <span>New</span>
+        <span className="hidden md:block">Invoice</span>
+      </div>
+    </Button>
   );
 }
 
@@ -21,6 +27,22 @@ export function UpdateInvoice({ id }: { id: string }) {
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
+    </Link>
+  );
+}
+
+export function ViewInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}`}
+      className="group hover:cursor-pointer"
+    >
+      <div className="flex justify-end">
+        <ChevronRightIcon
+          size={16}
+          className="text-primary transition-all duration-150 group-hover:scale-150"
+        />
+      </div>
     </Link>
   );
 }
