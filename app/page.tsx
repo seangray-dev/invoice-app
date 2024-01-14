f72751323f46e8e8dfc114e9aeb4f882a69446e1
Sean Gray <seangray.dev@gmail.com>
2024-01-14 05:41:22 -0500

    update import path for logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/components/slicebooks-logo';
+import AcmeLogo from '@/app/components/nav/slicebooks-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
